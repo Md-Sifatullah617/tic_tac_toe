0bde407ccdd572a2629ff056daa4dcdae70ffad4
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-09-30 01:26:39 +0600

    nothing

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Hi!"),
-        ],
-      ),
-    ));
+          child: Column(
+            children: [
+              Container(
+                height: Get.height * 0.2,
+                width: Get.width * 0.6,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white, width: 8),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(200),
+                        bottomRight: Radius.circular(200))),
+              ),
+              Text(
+                "Hi!",
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tic_tac_toe/homepage.dart';
 
 void main() {
@@ -9,11 +11,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'XYZ Gaming',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: GoogleFonts.sedgwickAveDisplay().fontFamily,
         useMaterial3: true,
       ),
       home: const MyHomePage(),
